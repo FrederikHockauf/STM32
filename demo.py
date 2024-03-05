6e9b946ec03ef16ae007162ee6e07c881bde9462
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-05 14:42:55 +0100

    Trying stuff

--- demo.py
+++ demo.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
-import serial   # pySerial
+import serial
 import sys
+import random
 
 if len(sys.argv) < 2:
   print("Usage: {0} [DEVICE]".format(sys.argv[0]))
   sys.exit(-1)
 
 dev = serial.Serial(sys.argv[1], 115200)
+
+opcodes = {
+  0 : "ADD",
+  1 : "MUL",
+  2 : "EXP",
+  3 : "INC",
+  4 : "RES",
+  5 : "TME",
+  255 : "ERR"}
+
+single_operand = ["INC"]
+
+
 
 while True:
   print("---| Begin loop |---")
@@ -15,9 +29,10 @@ while True:
   print(x.decode()) # 
   print("Flag 2 - We've printed it")
 
-  opcode = 3
-  opcode_data = opcode.to_bytes(1,'big')
-  dev.write(opcode_data)
+
+  opc = random.choice([0,1,2,3])
+  print(opcodes[opc], end =" ")
+  dev.write(opc.to_bytes(1, 'big'))
   y = dev.readline()
   print("Flag 3 - Read something")
   print(y.decode()) # 
