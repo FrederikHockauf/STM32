517c07e7847d9c7ab3efd459fe815c6de3ef6fed
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-08 08:40:50 +0100

    Printf with correct input

--- aes-enc.c
+++ aes-enc.c
@@ -67,9 +67,7 @@ int main()
 	char a_new = (char) a;
 	int b = 7;
 	char b_new = (char)b;
-	printf("a = ");
-	printf(b_new);
-	printf("\n");
+	printf("a = %d\n", b);
 	//uint8_t alle = Read4Bytes();
 	//uint8_t alle2 = Read4Bytes();
 
