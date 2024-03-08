462a3f0e76f910be2de56917aa37f8c692417385
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-08 10:28:50 +0100

    Better debugging

--- aes.py
+++ aes.py
@@ -54,13 +54,13 @@ for i in range(10):
 
   # compare reference and response block byte-by-byte
   for i in range(16):
-    print("\n----\nround 1")
+    print(f"\n----\nround {i}")
     print(f"dev = {ciphertext_dev[i]}")
     print(f"oth = {ciphertext_rev[i]}")
     if ciphertext_dev[i] != ciphertext_rev[i]:
-      print("\n----\nround 1")
-      print(f"dev = {ciphertext_dev[i]}")
-      print(f"oth = {ciphertext_rev[i]}")
+      #print("\n----\nround 1")
+      #print(f"dev = {ciphertext_dev[i]}")
+      #print(f"oth = {ciphertext_rev[i]}")
       #print("Error in encryption!\n")
   
   sys.exit(-1)
