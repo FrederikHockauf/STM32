e180cb15f92e76c3970b8ab4eaf287d937625dec
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-08 11:03:13 +0100

    Now for sure

--- aes.py
+++ aes.py
@@ -44,18 +44,28 @@ for i in range(10):
   sent_message = dev.read(16)
   print("Flag 7 - got data back")
 
+  print(f"dev = ")
   for i in range(16):
-    print(f"\n----\nround {i}")
-    print(f"dev = {sent_key[i]} ", end=" ")
-    #print(f"oth = {key[i]}")
+    print(f"{sent_key[i]} ", end=" ")
+  print("")
+
+  print(f"oth = ")
+  for i in range(16):
+    print(f"{key[i]} ", end=" ")
+  print("\n\n")
+
+  print(f"dev = ")
+  for i in range(16):
+    print(f"{sent_message[i]} ", end=" ")
+  print("")
+
+  print(f"oth = ")
+  for i in range(16):
+    print(f"{data[i]} ", end=" ")
+  print("\n\n")
   
 
   print("Flag 8 - done printing that now")
-
-  for i in range(16):
-    print(f"\n----\nround {i}")
-    print(f"dev = {sent_message[i]} ", end=" ")
-    #print(f"oth = {data[i]}")
     
   print("Flag 9 - reading data")
 
