2d44b7c524846b5cbf631e81c1ad399103717a0e
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-09 03:34:20 +0100

    Changed most parameters to be int512

--- multi-precision.c
+++ multi-precision.c
@@ -5,20 +5,20 @@
 
 typedef struct { int32_t a[11]; } int256;
 typedef struct { int32_t a[22]; } int512;
-const uint16_t TOTAL_BITS = 256;
+const uint16_t TOTAL_BITS = 512;
 const uint16_t TOTAL_LIMBS = 11;
 const uint8_t INT_BITS = 31; // Sign takes one bit
 const uint8_t RADIX = 24;
 
-int256 Karatsuba(int256 a, int256 b, int bits);
-int256 MPAAdd(int256 operand1, int256 operand2);
-int256 MPASubtraction(int256 operand1, int256 operand2);
-int256 MPABitshiftLeft(int256 number, int places);
-int256 MPABitshiftRight(int256 number, int places);
-int256 MPASchoolbookMultiplication(int256 operand1, int256 operand2);
-int256 MPAAbsoluteValue(int256 number);
-int256 ReducedRepresentation(int256 number);
-int256 Zero();
+int512 Karatsuba(int512 a, int512 b, int bits);
+int512 MPAAdd(int512 operand1, int512 operand2);
+int512 MPASubtraction(int512 operand1, int512 operand2);
+int512 MPABitshiftLeft(int512 number, int places);
+int512 MPABitshiftRight(int512 number, int places);
+int512 MPASchoolbookMultiplication(int512 operand1, int512 operand2);
+int512 MPAAbsoluteValue(int512 number);
+int512 ReducedRepresentation(int512 number);
+int256 Zero256();
 int512 Zero512();
 int512 ToInt512(int256 number);
 
@@ -52,6 +52,10 @@ int main()
         int finalTime;
         int512 result;
 
+        // Expand the operands to an int512 type
+        int512 expandedNumA = ToInt512(numA);
+        int512 expandedNumB = ToInt512(numB);
+
         // Perform either addition (0) or multiplication (1)
         if (opcode == 0)
         {
@@ -59,8 +63,7 @@ int main()
             int timeStart = hal_get_time();
             
             // Perform addition and convert the variable to a 512-variant
-            int256 total = MPAAdd(numA, numB);
-            result = ToInt512(ReducedRepresentation(total));
+            result = MPAAdd(expandedNumA, expandedNumB);
 
             // Stop the time and calculate the final time
             int timeStop = hal_get_time();
@@ -72,7 +75,8 @@ int main()
             int timeStart = hal_get_time();
             
             // Perform multiplication
-            int256 total = MPAAdd(numA, numB);
+            
+            int512 total = Karatsuba(expandedNumA, expandedNumB, TOTAL_BITS);
 
             // Stop the time and calculate the final time
             int timeStop = hal_get_time();
@@ -87,19 +91,6 @@ int main()
         uint8_t timeOpcode = 5;
         hal_putchar(timeOpcode);
         WriteBytes(finalTime, 4);
-
-        // Edit limbs to create non-zero numbers
-        //numA.a[0] = 0b11000000000000000000000000;
-        //numB.a[0] = 0b01000000000000000000000011;
-        //numB.a[1] = 0b00000000000000000000111011;
-        //numB.a[2] = 0b00000000000000000000000100;
-
-        // Calculate and print the product
-        //int256 result = Karatsuba(numA, numB, TOTAL_BITS);
-        
-        
-        // Write the encrypted message to the computer
-        //WriteBlock(encMessage);
     }
 
 
@@ -110,25 +101,25 @@ int main()
     return 0;
 }
 
-int256 Karatsuba(int256 a, int256 b, int bits)
+int512 Karatsuba(int512 a, int512 b, int bits)
 {
 	// Initialize the result and calculate the numbers' highest bit-representation in a potential next level Karatsuba
 	int bitsSection = bits / 2;
-	int256 result = Zero();
+	int512 result = Zero512();
 
 	// Split the numbers into highs and lows through bitshifting
-	int256 aHigh = MPABitshiftRight(a, bitsSection);
-	int256 bHigh = MPABitshiftRight(b, bitsSection);
-	int256 aLow = MPABitshiftRight(MPABitshiftLeft(a, TOTAL_BITS - bitsSection), TOTAL_BITS - bitsSection);
-	int256 bLow = MPABitshiftRight(MPABitshiftLeft(b, TOTAL_BITS - bitsSection), TOTAL_BITS - bitsSection);
+	int512 aHigh = MPABitshiftRight(a, bitsSection);
+	int512 bHigh = MPABitshiftRight(b, bitsSection);
+	int512 aLow = MPABitshiftRight(MPABitshiftLeft(a, TOTAL_BITS - bitsSection), TOTAL_BITS - bitsSection);
+	int512 bLow = MPABitshiftRight(MPABitshiftLeft(b, TOTAL_BITS - bitsSection), TOTAL_BITS - bitsSection);
 
 	// Declare and initialize components to be used in the final calculation
-	int256 high;
-	int256 low;
-	int256 medium;
-	int256 mediumHat;
-	int256 aDiff = MPASubtraction(aLow, aHigh);
-	int256 bDiff = MPASubtraction(bLow, bHigh);
+	int512 high;
+	int512 low;
+	int512 medium;
+	int512 mediumHat;
+	int512 aDiff = MPASubtraction(aLow, aHigh);
+	int512 bDiff = MPASubtraction(bLow, bHigh);
 
 	// If the numbers contain few enough bits to be contained in a limb after multiplcation use SB, otherwise recursive Karatsuba
 	if (bits <= INT_BITS / 2)
@@ -148,50 +139,50 @@ int256 Karatsuba(int256 a, int256 b, int bits)
 
 	// Constant time (?) comparison between medium and mediumHat to determine if it should be negated
 	int t;
-	for (int i = 0; i < TOTAL_LIMBS; i++)
+	for (int i = 0; i < (TOTAL_LIMBS * 2); i++)
 		if (medium.a[i] == mediumHat.a[i])
 			t += 0;
 		else
 			t = 0 + 1;
 
 	// Pick the medium value to use
-	int256 mediumType;
+	int512 mediumType;
     if (t == 0)
         mediumType = medium;
     else
         mediumType = mediumHat;
 
 	// Calculate the result
-	int256 parenthisSum = MPASubtraction(MPAAdd(low, high), mediumType);
+	int512 parenthisSum = MPASubtraction(MPAAdd(low, high), mediumType);
 	result = MPAAdd(MPAAdd(low, MPABitshiftLeft(parenthisSum, bits / 2)), MPABitshiftLeft(high, bits));
 
 	return result;
 }
 
-int256 MPAAdd(int256 operand1, int256 operand2)
+int512 MPAAdd(int512 operand1, int512 operand2)
 {
-	int256 result;
+	int512 result;
 
-	for (int i = 0; i < TOTAL_LIMBS; i++)
+	for (int i = 0; i < (TOTAL_LIMBS * 2); i++)
 		result.a[i] = operand1.a[i] + operand2.a[i];
 
 	return result;
 }
 
-int256 MPASubtraction(int256 operand1, int256 operand2)
+int512 MPASubtraction(int512 operand1, int512 operand2)
 {
 	// Calculate operand2's negative version by finding its two's complement (negating and adding 1)
-	for (int i = 0; i < TOTAL_LIMBS; i++)
-		operand2.a[i] = !operand2.a[i];
+	for (int i = 0; i < (TOTAL_LIMBS * 2); i++)
+		operand2.a[i] = ~operand2.a[i];
 	operand2.a[0] += 1;
 
 	// Calculate subtraction by adding the negative of operand 2 to operand 1
-	int256 result = MPAAdd(operand1, operand2);
+	int512 result = MPAAdd(operand1, operand2);
 	result = ReducedRepresentation(result);
 	return result;
 }
 
-int256 MPABitshiftLeft(int256 number, int places)
+int512 MPABitshiftLeft(int512 number, int places)
 {
 	// If we want to shift by more places than in a limb, we shift by the size of a limb and save the places left to shift with
 	int remainingPlaces = places;
@@ -200,9 +191,9 @@ int256 MPABitshiftLeft(int256 number, int places)
 	remainingPlaces -= places;
 
 	// Just shift the bottom-most limb as we don't need to save anything
-	number.a[TOTAL_LIMBS - 1] <<= places;
+	number.a[(TOTAL_LIMBS * 2) - 1] <<= places;
 
-	for (int i = 8; i >= 0; i--)
+	for (int i = (TOTAL_LIMBS * 2) - 2; i >= 0; i--)
 	{
 		// Find the portion of the i'th limb that will get deleted and save it
 		uint32_t mask = (((1 << RADIX) - 1) >> (RADIX - places)) << (RADIX - places);
@@ -220,7 +211,7 @@ int256 MPABitshiftLeft(int256 number, int places)
 	return number;
 }
 
-int256 MPABitshiftRight(int256 number, int places)
+int512 MPABitshiftRight(int512 number, int places)
 {
 	// If we want to shift by more places than in a limb, we shift by the size of a limb and save the places left to shift with
 	int remainingPlaces = places;
@@ -231,7 +222,7 @@ int256 MPABitshiftRight(int256 number, int places)
 	// Just shift the bottom-most limb as we don't need to save anything
 	number.a[0] >>= places;
 
-	for (int i = 1; i < TOTAL_LIMBS; i++)
+	for (int i = 1; i < (TOTAL_LIMBS * 2); i++)
 	{
 		// Find the portion of the i'th limb that will get deleted and save it
 		uint32_t mask = (1 << places) - 1;
@@ -249,24 +240,24 @@ int256 MPABitshiftRight(int256 number, int places)
 	return number;
 }
 
-int256 MPASchoolbookMultiplication(int256 operand1, int256 operand2)
+int512 MPASchoolbookMultiplication(int512 operand1, int512 operand2)
 {
 	// Set the result to be the limb-wise schoolbook multiplication of the operands
-	int256 result;
-	for (int i = 0; i < TOTAL_LIMBS; i++)
+	int512 result;
+	for (int i = 0; i < (TOTAL_LIMBS * 2); i++)
 		result.a[i] = operand1.a[i] * operand2.a[i];
 
 	return result;
 }
 
-int256 MPAAbsoluteValue(int256 number)
+int512 MPAAbsoluteValue(int512 number)
 {
 	return number;
 }
 
-int256 ReducedRepresentation(int256 number)
+int512 ReducedRepresentation(int512 number)
 {
-	for (int i = 0; i < (TOTAL_LIMBS - 1); i++)
+	for (int i = 0; i < ((TOTAL_LIMBS * 2) - 1); i++)
 	{
 		uint32_t carry = number.a[i] >> RADIX;
 		number.a[i + 1] += carry;
@@ -277,7 +268,7 @@ int256 ReducedRepresentation(int256 number)
 	return number;
 }
 
-int256 Zero()
+int256 Zero256()
 {
 	int256 number;
 
@@ -328,7 +319,7 @@ void WriteBytes(int variable, int bytes)
 
 int256 ReadInt256()
 {
-    int256 number = Zero();
+    int256 number = Zero256();
 
     number.a[TOTAL_LIMBS - 1] = ReadBytes(2);
     for (int limb = (TOTAL_LIMBS - 2); limb > -1; limb--)
