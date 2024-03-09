c680a0c07a10537c6c3ab9ae1454ce4f573ecd18
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-09 06:12:23 +0100

    Next limb multiplication debug

--- multi.py
+++ multi.py
@@ -39,7 +39,7 @@ for i in range(10):
   a = random.randint(0, 1<<256-1)
   b = random.randint(0, 1<<256-1)
   a = 0b0000000000000000000000000001
-  b = 0b10000000000000
+  b = 0b1000000000000000000000000000
   #print(f"a = {a}")
   #print(f"b = {b}")
   #print(f"a binary = {bin(a)}")
