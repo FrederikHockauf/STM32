c225fbe7680ea43b7bf937de8edbe44af571ceb8
Frederik Hockauf <125813714+FrederikHockauf@users.noreply.github.com>
2024-03-09 06:48:01 +0100

    Try bitshifting

--- multi-precision.c
+++ multi-precision.c
@@ -70,7 +70,8 @@ int main()
         {
             // Perform multiplication
             timeStart = hal_get_time();
-            result = MPASchoolbookMultiplication(expandedNumA, expandedNumB);
+			result = MPABitshiftLeft(expandedNumA, rounds);
+            //result = MPASchoolbookMultiplication(expandedNumA, expandedNumB);
             result = ReducedRepresentation(result);
             //result = Karatsuba(expandedNumA, expandedNumB, TOTAL_BITS);
             timeStop = hal_get_time();
